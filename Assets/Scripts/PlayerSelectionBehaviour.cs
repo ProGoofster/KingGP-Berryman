46fd5f7afac1b0e0a2ec6800e85023e4a37cf1a7
Jake Berryman <59934842+ProGoofster@users.noreply.github.com>
2025-02-17 15:54:46 -0600

    2/17 class work commit

---
+++ Assets/Scripts/PlayerSelectionBehaviour.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerSelectionBehaviour : MonoBehaviour
+{
+    public Pins pinsDB;
+
+    public static int selection = 0;
+    public SpriteRenderer sprite;
+    public TMP_Text nameLabel;
+    
+    void UpdateCharacter() {
+        Pin current = pinsDB.getPin(selection);
+        sprite.sprite = current.prefab.GetComponent<SpriteRenderer>().sprite;
+        nameLabel.SetText(current.name);
+    }
+
+    public void next(){
+        int numberPins = pinsDB.getCount();
+        if (selection < numberPins-1) {
+            selection = selection + 1;
+        }
+        UpdateCharacter();
+    }
+
+    public void previous(){
+        if(selection > 0){
+            selection = selection - 1;
+        }
+        UpdateCharacter();
+    }
+}
